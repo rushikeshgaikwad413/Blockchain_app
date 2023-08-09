31f8c50c527663f7cdc42feb2313d46319148342
rushiekshgaikwad413 <rushikeshgaikwad413@gmail.com>
2023-08-09 10:26:29 +0530

    Initial commit

---
+++ blockchain/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            {'rushikeshg'}
+            © 2023 CRYPTO DASHBOARD
+        </footer>
+    );
+}
+
+export default Footer;
